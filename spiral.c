4080686797ab65d52af1e992c4dd8b5b7b949514
Long <tqlong@gmail.com>
2020-04-01 12:44:59 +0700

    add spiral problem

---
+++ spiral.c
@@ -0,0 +1,6 @@
+// Input: 2 m: so dong, n: so cot
+// Output: 1 bang so xoay tron oc theo chieu kim dong ho co m dong, n cot, cac so tu 1 -> m*n
+// VD: m = 3, n = 4
+//  1  2  3  4
+// 10 11 12  5
+//  9  8  7  6
